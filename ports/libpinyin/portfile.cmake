c36d3c4e561d760473082bced5bd8eb68c8fcbc9
etorth <anhonghe@gmail.com>
2026-06-18 02:19:36 -0700

    Revert "Trial narrow MinGW libpinyin build"
    This reverts commit 7a7f64a412c6831ab0764deb84b06dffba1f7cf6.

--- ports/libpinyin/portfile.cmake
+++ ports/libpinyin/portfile.cmake
@@ -1,5 +1,3 @@
-set(VCPKG_BUILD_TYPE release)
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO etorth/libpinyin
@@ -30,10 +28,7 @@ vcpkg_configure_make(
         --disable-dependency-tracking
 )
 
-vcpkg_install_make(
-    BUILD_TARGET "SUBDIRS=src utils data\;MAKEOVERRIDES=\;all-recursive"
-    INSTALL_TARGET "SUBDIRS=src data\;MAKEOVERRIDES=\;install-recursive"
-)
+vcpkg_install_make()
 
 vcpkg_fixup_pkgconfig()
 
